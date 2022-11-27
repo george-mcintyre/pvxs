ac4ba1dee8645e97fd58b854361b6ee7529ddbb9
george-mcintyre <george@level-n.com>
2022-11-27 16:24:40 +0100

    Complete implementation of qsrv except for groups parameter

--- qsrv/qsrvMain.h
+++ qsrv/qsrvMain.h
@@ -14,6 +14,7 @@
 #define DEFAULT_INITIALISATION_FILENAME IOC_SERVER_NAME ".dbd"
 #define INITIALISATION_FILENAME "dbd" OSI_PATH_SEPARATOR DEFAULT_INITIALISATION_FILENAME
 #define FULL_PATH_TO_INITIALISATION_FILE EPICS_BASE OSI_PATH_SEPARATOR INITIALISATION_FILENAME
+#define RELATIVE_PATH_TO_INITIALISATION_FILE ".." OSI_PATH_SEPARATOR ".." OSI_PATH_SEPARATOR INITIALISATION_FILENAME
 // automatically generated from dbd file.  This must match the initialization filename you choose above.  xxxx_registerRecordDeviceDriver()
 extern "C" int qsrv_registerRecordDeviceDriver(struct dbBase* pdbbase);
 
@@ -21,5 +22,6 @@ extern "C" int qsrv_registerRecordDeviceDriver(struct dbBase* pdbbase);
 // Must match with `xxxx_DBD = xxxx.dbd` in your Makefile
 #define SHUTDOWN_FILENAME "db" OSI_PATH_SEPARATOR IOC_SERVER_NAME "Exit.db"
 #define FULL_PATH_TO_EXIT_FILE EPICS_BASE OSI_PATH_SEPARATOR SHUTDOWN_FILENAME
+#define RELATIVE_PATH_TO_SHUTDOWN_FILE ".." OSI_PATH_SEPARATOR ".." OSI_PATH_SEPARATOR SHUTDOWN_FILENAME
 
 #endif //PVXS_QSRVMAIN_H
