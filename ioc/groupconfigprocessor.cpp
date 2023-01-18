0f27da6f1797fd18d2e98c5e7fb6ca66ef7a6c1b
george-mcintyre <george@level-n.com>
2023-01-19 00:14:52 +0100

    Trigger github actions

--- ioc/groupconfigprocessor.cpp
+++ ioc/groupconfigprocessor.cpp
@@ -2,6 +2,7 @@
  * Copyright - See the COPYRIGHT that is included with this distribution.
  * pvxs is distributed subject to a Software License Agreement found
  * in file LICENSE that is included with this distribution.
+ *
  */
 
 #include <iostream>
