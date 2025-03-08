5a9239af0254007a3fc68e54ed4f35183aba8175
george-mcintyre <george@level-n.com>
2025-03-08 21:43:47 +0100

    "Remove explicit username from 'docker login' command in build scripts"

--- example/docker/epics-base/build.sh
+++ example/docker/epics-base/build.sh
@@ -2,5 +2,5 @@
 
 # Build the Docker image and tag it, login, then push it
 docker build --pull -t ${DOCKER_USERNAME}/epics-base:latest . && \
-docker login ${DOCKER_USERNAME} && \
+docker login && \
 docker push ${DOCKER_USERNAME}/epics-base:latest
--- example/docker/spva_std/build.sh
+++ example/docker/spva_std/build.sh
@@ -2,5 +2,5 @@
 
 # Build the Docker image and tag it, login, then push it
 docker build  --build-arg DOCKER_USERNAME=${DOCKER_USERNAME} --pull -t ${DOCKER_USERNAME}/spva_std:latest . && \
-docker login ${DOCKER_USERNAME} && \
+docker login && \
 docker push ${DOCKER_USERNAME}/spva_std:latest
