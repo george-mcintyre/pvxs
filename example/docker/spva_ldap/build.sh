11f89d87f744e437325d501dd8b7186a36e1cb37
george-mcintyre <george@level-n.com>
2025-03-08 21:39:19 +0100

    "Replace `--no-cache` with `--pull` in Docker build scripts to ensure fetching the latest base image versions."

--- example/docker/spva_ldap/build.sh
+++ example/docker/spva_ldap/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Build the Docker image and tag it, login, then push it
-docker build  --build-arg DOCKER_USERNAME=${DOCKER_USERNAME} --no-cache -t ${DOCKER_USERNAME}/spva_ldap:latest . && \
+docker build  --build-arg DOCKER_USERNAME=${DOCKER_USERNAME} --pull -t ${DOCKER_USERNAME}/spva_ldap:latest . && \
 docker login ${DOCKER_USERNAME} && \
 docker push ${DOCKER_USERNAME}/spva_ldap:latest
