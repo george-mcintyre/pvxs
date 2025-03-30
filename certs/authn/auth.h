37a22582c02dac3232377020a3d9df4a6e84da7c
george-mcintyre <george@level-n.com>
2025-03-30 06:09:09 +0200

    Fix null certificate handling when retrieving 'not_after_time'
    - Added a conditional check to handle cases where `cert_data.cert` is null, preventing potential runtime errors.

--- certs/authn/auth.h
+++ certs/authn/auth.h
@@ -484,7 +484,7 @@ int runAuthenticator(int argc, char *argv[], std::function<void(ConfigT &, AuthT
         try {
             auto new_cert_data = IdFileFactory::create(tls_keychain_file, tls_keychain_pwd)->getCertDataFromFile();
             const auto now = time(nullptr);
-            const auto not_after_time = CertFactory::getNotAfterTimeFromCert(new_cert_data.cert);
+            const auto not_after_time = (!cert_data.cert) ? 0 :  CertFactory::getNotAfterTimeFromCert(new_cert_data.cert);
             if (not_after_time > now) {
                 cert_data = std::move(new_cert_data);
             }
